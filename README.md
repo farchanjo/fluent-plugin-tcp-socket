d7b3f5fa5305b68dfde7c163a20ee1bfae30845d
root <root@mail.archanjo.eti.br>
2019-09-15 14:27:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# fluent-plugin-tcp-socket
+
+[Fluentd](https://fluentd.org/) output plugin to do something.
+
+TODO: write description for you plugin.
+
+## Installation
+
+### RubyGems
+
+```
+$ gem install fluent-plugin-tcp-socket
+```
+
+### Bundler
+
+Add following line to your Gemfile:
+
+```ruby
+gem "fluent-plugin-tcp-socket"
+```
+
+And then execute:
+
+```
+$ bundle
+```
+
+## Configuration
+
+You can generate configuration template:
+
+```
+$ fluent-plugin-config-format output tcp-socket
+```
+
+You can copy and paste generated documents here.
+
+## Copyright
+
+* Copyright(c) 2019- TODO: Write your name
+* License
+  * Apache License, Version 2.0
---
+++ lib/fluent/plugin/out_tcp_socket.rb
@@ -0,0 +1,70 @@
+#
+# Copyright 2019- TODO: Write your name
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "fluent/plugin/output"
+
+module Fluent
+  module Plugin
+    class TcpSocketOutput < Fluent::Plugin::Output
+      Fluent::Plugin.register_output("tcp_socket", self)
+      @@socket = nil
+      config_param :hostname, :string, default: "localhost"
+      config_param :port, :integer, default: 2238
+      @@execution_thread = nil
+      @@queue = Queue.new
+
+      def start
+         super
+         log.info "TCP Plugin started with %s:%s" % [hostname, port]
+         @@execution_thread = Thread.new { start_service_thread }
+      end
+
+      def stop
+         if @@execution_thread != nil
+            Thread.kill(@@execution_thread)
+            log.info "TCP Plugin thread has been killed"
+         end
+         @@queue.close
+         log.info "Queue is closed"
+      end
+
+      def process(tag, es)
+         es.each do |time, record|
+            @@queue.enq("%s\r\n" % [record])
+         end
+      end
+
+      def start_service_thread
+         log.info "TCP Plugin thread started for %s:%s" % [hostname, port]
+         while record = @@queue.deq
+            begin
+              get_client.puts record
+            rescue
+              log.error "client error %s" %[$!]
+              @@socket = nil
+            end
+         end
+      end
+
+      def get_client
+         if @@socket == nil
+            @@socket = TCPSocket.open(hostname, port)
+            log.info "Socket established for %s:%s" % [hostname, port]
+         end
+         return @@socket
+      end
+    end
+  end
+end
